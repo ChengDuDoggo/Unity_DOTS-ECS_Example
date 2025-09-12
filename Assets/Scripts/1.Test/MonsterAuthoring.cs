8d5b25639f5c6380daece4a94ed7ada00a0906b3
Doggo <g382782974@gmail.com>
2025-09-12 16:03:23 +0800

    Baker

---
+++ Assets/Scripts/1.Test/MonsterAuthoring.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class MonsterAuthoring : MonoBehaviour
+{
+    public float hp = 100;
+    public float moveSpeed = 1;
+    //烘培器(将MonoMonoBehaviour对象烘培到ECS中)
+    public class MonsterBaker : Baker<MonsterAuthoring>
+    {
+        public override void Bake(MonsterAuthoring authoring)
+        {
+            //获取实体(该实体是动态的,因此TransformUsageFlags.Dynamic,Dynamic会自动帮助实体添加LocalTransform数据组件)
+            Entity monsterEntity = GetEntity(TransformUsageFlags.Dynamic);
+            //增加数据组件并赋值
+            AddComponent<MonsterData>(monsterEntity, new MonsterData()
+            {
+                hp = authoring.hp,
+                moveSpeed = authoring.moveSpeed
+            });
+        }
+    }
+}
--- Assets/Scripts/1.Test/TestSystem.cs
+++ Assets/Scripts/1.Test/TestSystem.cs
@@ -12,6 +12,7 @@ public readonly partial struct MonsterAspect : IAspect
 public struct MonsterData : IComponentData
 {
     public float hp;
+    public float moveSpeed;
 }
 //定义一个DOTS的系统(实现逻辑)
 public partial struct MonsterSystem : ISystem
@@ -19,10 +20,14 @@ public partial struct MonsterSystem : ISystem
     //DOTS/ECS System的三个生命周期函数
     public void OnCreate(ref SystemState state)
     {
+        //正常来说不会在这里直接创建实体,而是通过在Unity中创建MonoBehaviour，Authoring(Authoring是MonoBehaviour的类)和Baker(烘焙器)来创建实体
         //创建一个Monster Entity(实体)并且在创建时添加MonsterData和LocalTransform(Unity自带)数据组件
-        Entity monster = state.EntityManager.CreateEntity(typeof(MonsterData), typeof(LocalTransform));
+        //Entity monster = state.EntityManager.CreateEntity(typeof(MonsterData), typeof(LocalTransform));
         //设置实体的MonsterData数据组件的hp值为100
-        state.EntityManager.SetComponentData(monster, new MonsterData { hp = 100 });
+        //state.EntityManager.SetComponentData(monster, new MonsterData { hp = 100 });
+
+        //Baker方式烘培了一个Mono,直接将Mono转换为一个实体,System直接可以筛选遍历，不需要再创建实体操作
+
     }
     public readonly void OnUpdate(ref SystemState state)
     {
@@ -30,16 +35,16 @@ public partial struct MonsterSystem : ISystem
         //筛选遍历
         float3 dir = new(0, 0, 1);//方向
         //筛选遍历拥有MonsterData和LocalTransform数据组件的实体
-        foreach ((RefRW<MonsterData> monsterData, RefRW<LocalTransform> localTransfrom) in SystemAPI.Query<RefRW<MonsterData>, RefRW<LocalTransform>>())
-        {
-            //读写其中的数据组件以更改数据实现逻辑
-            localTransfrom.ValueRW.Position += dir * SystemAPI.Time.DeltaTime;//移动
-            monsterData.ValueRW.hp -= SystemAPI.Time.DeltaTime;//掉血
-        }
+        //foreach ((RefRW<MonsterData> monsterData, RefRW<LocalTransform> localTransfrom) in SystemAPI.Query<RefRW<MonsterData>, RefRW<LocalTransform>>())
+        //{
+        //    //读写其中的数据组件以更改数据实现逻辑
+        //    localTransfrom.ValueRW.Position += dir * SystemAPI.Time.DeltaTime;//移动
+        //    monsterData.ValueRW.hp -= SystemAPI.Time.DeltaTime;//掉血
+        //}
         //使用封装数据组件的方式筛选遍历
         foreach (MonsterAspect monster in SystemAPI.Query<MonsterAspect>())
         {
-            monster.localTransform.ValueRW.Position += dir * SystemAPI.Time.DeltaTime;//移动
+            monster.localTransform.ValueRW.Position += monster.monsterData.ValueRW.moveSpeed * SystemAPI.Time.DeltaTime * dir;//移动
             monster.monsterData.ValueRW.hp -= SystemAPI.Time.DeltaTime;//掉血
         }
     }
