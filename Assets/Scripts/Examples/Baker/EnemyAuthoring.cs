f76d6b87342e62d5ff3d7389765036abbb18219f
Doggo <g382782974@gmail.com>
2025-09-29 15:18:58 +0800

    Bee

---
+++ Assets/Scripts/Examples/Baker/EnemyAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class EnemyAuthoring : MonoBehaviour
+{
+    public Vector3 scale = Vector3.one;
+    public float moveSpeed = 4.0f;
+    public class EnemyBaker : Baker<EnemyAuthoring>
+    {
+        public override void Bake(EnemyAuthoring authoring)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Examples/ECS/Animation/AnimationSystem.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+
+public partial struct AnimationSystem : ISystem
+{
+    public void OnUpdate(ref SystemState state)
+    {
+        new AnimationJob
+        {
+            deltaTime = state.WorldUnmanaged.Time.DeltaTime
+        }.ScheduleParallel();
+    }
+    [BurstCompile]
+    public partial struct AnimationJob : IJobEntity
+    {
+        public float deltaTime;
+        public readonly void Execute(ref AnimationFrameIndex frameIndex, in AnimationSharedData animationData)
+        {
+            float newIndex = frameIndex.value + animationData.frameRate * deltaTime;
+            while (newIndex > animationData.frameMaxIndex)
+            {
+                newIndex -= animationData.frameMaxIndex;
+            }
+            frameIndex.value = newIndex;
+        }
+    }
+}
